1a79401bd9f1c0c4bd1ab3c986af8f2e8933e1e3
Nathan Jay <nholcomb@wisc.edu>
2016-08-24 16:09:43 -0500

    Fixed an issue preventing LLVMInit from being called in release mode; fixed tabbing in altered functions.

--- fleece/decoders/llvm_x86_64.C
+++ fleece/decoders/llvm_x86_64.C
@@ -20,6 +20,7 @@
 
 #include "Normalization.h"
 #include "Mystring.h"
+#include <iostream>
 #include <iomanip>
 #include "llvm_common.h"
 
@@ -32,11 +33,22 @@ static const char* LLVMCallback(void* info, uint64_t refVal, uint64_t* refType, 
 
 int llvm_x86_64_decode(char* inst, int nBytes, char* buf, int bufLen) {
 
-   static LLVMDisasmContextRef disasm = LLVMCreateDisasm("x86_64-linux-gnu", nullptr, 0, nullptr, LLVMCallback);
+    static LLVMDisasmContextRef disasm = LLVMCreateDisasm(
+        "x86_64-linux-gnu", 
+        nullptr, 
+        0, 
+        nullptr, 
+        LLVMCallback);
 
-   size_t bytesUsed = LLVMDisasmInstruction(disasm, (uint8_t*)inst, nBytes, 0, buf, (size_t)bufLen);
+    size_t bytesUsed = LLVMDisasmInstruction(
+        disasm, 
+        (uint8_t*)inst, 
+        nBytes, 
+        0, 
+        buf, 
+        (size_t)bufLen);
 
-   return !bytesUsed;
+    return !bytesUsed;
 }
 
 void llvm_x86_64_norm(char* buf, int bufLen) {
