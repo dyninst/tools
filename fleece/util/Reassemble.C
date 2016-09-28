41d637d9b0cfa77abf9bfe53c6fc307243dc6af4
Nathan Jay <nholcomb@wisc.edu>
2016-09-28 11:22:06 -0500

    Reassembly updated to handle a single assembler option

--- fleece/util/Reassemble.C
+++ fleece/util/Reassemble.C
@@ -1,58 +1,108 @@
 
 #include "Reassemble.h"
 
-char reassemble(const char* bytes, int nBytes, const char* str, FILE* tmp, const char* tmpname) {
+char reassemble(const char* bytes, int nBytes, const char* str, FILE* tmp, 
+        const char* tmpname, char* byteBuf, int bufLen, int* outputLen) {
 
-    char arrBuf[BUFFER_SIZE];
+    static const char* as = Options::get("-as=");
+    static const char* asOpts = Options::get("-asopt=");
+    if (as == NULL) {
+        std::cerr << "Must specify assembler with \"-as=\"\n";
+        exit(-1);
+    }
+
+    char arrBuf[REASM_BUF_LEN];
     char* buf = &arrBuf[0];
 
-    snprintf(buf, BUFFER_SIZE, ".global main\n\nmain:\n\t%s", str);
+    snprintf(buf, REASM_BUF_LEN, ".global main\n\nmain:\n\t%s", str);
     writeStrToFile(tmpname, 0, buf);
-    snprintf(buf, BUFFER_SIZE, "as -o %s.o %s 2>as.out", tmpname, tmpname);
+    if (asOpts == NULL) {
+        snprintf(buf, REASM_BUF_LEN,
+            "%s -o %s.o %s 2>as.out", as, tmpname, tmpname);
+    } else {
+        snprintf(buf, REASM_BUF_LEN,
+            "%s %s -o %s.o %s 2>as.out", as, asOpts, tmpname, tmpname);
+    }
     int rc = system(buf);
 
     if (rc != 0) {
       return 'E';
     }
 
-    snprintf(buf, BUFFER_SIZE, "objdump -d %s.o > %s.tmp", tmpname, tmpname);
-    system(buf);
+    *outputLen = readReassembledBytes(tmpname, byteBuf, bufLen);
+    if (*outputLen > nBytes || memcmp(byteBuf, bytes, *outputLen)) {
+        return 'D';
+    }
+    return 'S';
+}
 
-    snprintf(buf, BUFFER_SIZE, "%s.tmp", tmpname);
-    FILE* bytef = fopen(buf, "r+");
+int readReassembledBytes(const char* filename, char* outBytes, int bufLen) {
+   
+    static const char* objdump = Options::get("-objdump=");
+    if (objdump == NULL) {
+        std::cerr << "Must specify objdump with \"-objdump=\"\n";
+        exit(-1);
+    }
+    
+    char buf[REASM_BUF_LEN];
+    char* str = &buf[0];
+    snprintf(str, REASM_BUF_LEN, "%s -d %s.o > %s.tmp", objdump, filename,
+        filename);
+
+    system(str);
+    snprintf(str, REASM_BUF_LEN, "%s.tmp", filename);
+    FILE* bytef = fopen(str, "r+");
     assert(bytef != NULL);
 
-    int flen = fread(buf, 1, BUFFER_SIZE, bytef);
+    int flen = fread(str, 1, REASM_BUF_LEN, bytef);
     assert(flen > 0);
 
     fclose(bytef);
 
-    char* cur = buf;
-    char* end = buf + flen;
+    char* cur = str;
+    char* end = str + flen;
 
-    int tabCount = 0;
-    while (cur < end && tabCount < GOAL_TAB_COUNT) {
-        if (*cur == '\t') {
-            tabCount++;
+    int colonCount = 0;
+    while (cur < end && colonCount < BYTE_COLON_COUNT) {
+        if (*cur == ':') {
+            colonCount++;
         }
+        cur++;
+    }
+    while (isspace(*cur)) {
         cur++;
     }
 
     int curByte = 0;
     char* byteStart = cur;
-    while (!isspace(*cur) && cur < end - 1 && curByte < nBytes) {
+    if (Architecture::name == "aarch64") {
+        cur += 6;
+    }
+    while (cur < end - 1 && curByte < bufLen && cur >= byteStart) {
         char c = (getCharHexVal(*cur) << 4) + getCharHexVal(*(cur + 1));
-        if (c != bytes[curByte]) {
-            return 'D';
-        }
+        outBytes[curByte] = c;
         curByte++;
-        cur += 3;
+        if (Architecture::name == "aarch64") {
+            cur -= 2;
+        } else {
+            cur += 3;
+            if (isspace(*cur) || curByte % 7 == 0) {
+                while (cur < end - 1 && *cur != '\n') {
+                    cur++;
+                }
+                while(cur < end - 1 && *cur != ':') {
+                    cur++;
+                }
+                if (*cur == ':') {
+                    cur++;
+                }
+                while(cur < end - 1 && isspace(*cur)) {
+                    cur++;
+                }
+            }
+        }
     }
 
-    if (cur >= end - 1 && curByte != nBytes) {
-        return 'D';
-    }
-
-    return 'S';
+    return curByte;
 }
 
