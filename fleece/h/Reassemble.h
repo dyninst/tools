f1b94bf448cb1f4ea8977169b4752b6f82228dfd
Nathan Jay <nholcomb@wisc.edu>
2016-09-29 15:35:57 -0500

    Reassembly uses execve for as, still needs work for objdump

--- fleece/h/Reassemble.h
+++ fleece/h/Reassemble.h
@@ -3,12 +3,14 @@
 #define REASSEMBLE_H_
 
 #include <assert.h>
+#include <errno.h>
 #include <inttypes.h>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include "Architecture.h"
 #include "Options.h"
--- fleece/util/Reassemble.C
+++ fleece/util/Reassemble.C
@@ -1,11 +1,67 @@
 
 #include "Reassemble.h"
 
+int getOptArgCount(const char* opts) {
+    
+    if (opts == NULL)
+        return 0;
+
+    int optArgCount = 1;
+    while(*opts) {
+        if (*opts == ',')
+            optArgCount++;
+        opts++;
+    }
+    
+    return optArgCount;
+}
+
+void fillOptArgs(char** args, const char* opts) {
+    const char* argStart = opts;
+    const char* argEnd = opts;
+    int curArg = 0;
+    while (*argEnd) {
+        if (*argEnd == ',') {
+            args[curArg] = strndup(argStart, argEnd - argStart);
+            argStart = argEnd + 1;
+            curArg++;
+        }
+        argEnd++;
+    }
+    args[curArg] = strdup(argStart);
+}
+
+char** makeAsArgs(const char* as, const char* asOpts, 
+        const char* tmpname) {
+
+    const int nNeededArgs = 5;
+    int nOptArgs = getOptArgCount(asOpts);
+    
+    char** result = (char**)malloc(sizeof(*result)*(nOptArgs + nNeededArgs));
+    assert(result != NULL);
+
+    result[0] = strdup(as);
+    result[1] = strdup(tmpname);
+    result[2] = strdup("-o");
+    int outputFilenameLen = strlen(tmpname) + strlen(".o") + 1;
+    result[3] = (char*)malloc(outputFilenameLen);
+    assert(result[3] != NULL);
+    snprintf(result[3], outputFilenameLen, "%s.o", tmpname);
+
+    if (nOptArgs > 0) {
+        fillOptArgs(&result[4], asOpts);
+    }
+
+    result[nOptArgs + nNeededArgs - 1] = NULL;
+    return result;
+}
+
 char reassemble(const char* bytes, int nBytes, const char* str, FILE* tmp, 
         const char* tmpname, char* byteBuf, int bufLen, int* outputLen) {
 
     static const char* as = Options::get("-as=");
     static const char* asOpts = Options::get("-asopt=");
+    static char** asArgv = makeAsArgs(as, asOpts, tmpname);
     if (as == NULL) {
         std::cerr << "Must specify assembler with \"-as=\"\n";
         exit(-1);
@@ -16,17 +72,22 @@ char reassemble(const char* bytes, int nBytes, const char* str, FILE* tmp,
 
     snprintf(buf, REASM_BUF_LEN, ".global main\n\nmain:\n\t%s", str);
     writeStrToFile(tmpname, 0, buf);
-    if (asOpts == NULL) {
-        snprintf(buf, REASM_BUF_LEN,
-            "%s -o %s.o %s 2>as.out", as, tmpname, tmpname);
+    
+    pid_t pid = fork();
+    if (pid == -1) {
+        std::cerr << "ERROR: failed to fork() for reassembly\n";
+        exit(-1);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        if (status != 0) {
+            return 'E';
+        }
     } else {
-        snprintf(buf, REASM_BUF_LEN,
-            "%s %s -o %s.o %s 2>as.out", as, asOpts, tmpname, tmpname);
-    }
-    int rc = system(buf);
-
-    if (rc != 0) {
-      return 'E';
+        char* envp = NULL;
+        execve(asArgv[0], asArgv, &envp);
+        std::cout << strerror(errno) << "\n";
+        assert(false && "execve() should never return");
     }
 
     *outputLen = readReassembledBytes(tmpname, byteBuf, bufLen);
