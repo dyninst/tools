fd60fdd330c0d267a0bb3c2a228e8b21166ec672
Nathan Jay <nholcomb@wisc.edu>
2016-09-29 12:00:24 -0500

    Added reassembly header

---
+++ fleece/h/Reassemble.h
@@ -0,0 +1,25 @@
+
+#ifndef REASSEMBLE_H_
+#define REASSEMBLE_H_
+
+#include <assert.h>
+#include <inttypes.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "Architecture.h"
+#include "Options.h"
+#include "StringUtils.h"
+
+#define REASM_FILENAME "tmp_asm_file_2465254685.s"
+#define REASM_BUF_LEN 512
+#define BYTE_COLON_COUNT 4
+
+char reassemble(const char* bytes, int nBytes, const char* str, FILE* tmp, 
+    const char* tmpname, char* byteBuf, int bufLen, int* outputLen);
+int readReassembledBytes(const char* filename, char* outBytes, int bufLen);
+
+#endif // REASSEMBLE_H_
