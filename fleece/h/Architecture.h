18a2a19c65cab03868b198062ed03a71434f98e6
Nathan Jay <nholcomb@wisc.edu>
2016-11-21 13:22:36 -0600

    Architecture now includes maximum instruction length

--- fleece/h/Architecture.h
+++ fleece/h/Architecture.h
@@ -1,16 +1,18 @@
 #ifndef _ARCHITECTURE_H_
 #define _ARCHITECTURE_H_
 
+#include <iostream>
 #include <string>
 #include "Alias.h"
 #include "FieldList.h"
 #include "RegisterSet.h"
 
 namespace Architecture {
-   extern std::string name;
-   void init(const char* arch);
-   void replaceRegSets(FieldList& fl);
-   void destroy();
+    extern int maxInsnLen;
+    extern std::string name;
+    void init(const char* arch);
+    void replaceRegSets(FieldList& fl);
+    void destroy();
 }
 
 #endif /* _ARCHITECTURE_H_ */
