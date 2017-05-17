09ae81ae26607e982c5aaea477d3a890ca4bfdc9
Nathan Jay <nholcomb@wisc.edu>
2017-05-17 13:56:48 -0500

    Improved documentation for Mask class

--- fleece/h/Mask.h
+++ fleece/h/Mask.h
@@ -1,3 +1,23 @@
+
+/*
+ * See fleece/COPYRIGHT for copyright information.
+ *
+ * This file is a part of Fleece.
+ *
+ * Fleece is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *  
+ * This software is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, see www.gnu.org/licenses
+*/
+
 #ifndef _MASK_H_
 #define _MASK_H_
 
@@ -9,26 +29,98 @@
 #define MASK_SYMBOL_CLR_BIT '0'
 #define MASK_SYMBOL_INC_BIT 'n'
 
+/*
+ * A Mask object can be applied to a buffer of bytes to set, clear and increment
+ * combinations of bits. Masks are constructred from strings containing the
+ * MASK_SYMBOL values defined above.
+ *
+ * Example usage:
+ *
+ * Mask m = 
+ *   Mask("11110000nnnnxxxx")
+ * m.apply(0100011110101110)
+ * result: 1111000000001110
+ *
+ * Each character of the mask containing a '1' signifies a bit of the output
+ * should be set to 1. Each character of the mask containing a '0' signifies a
+ * bit of the output that should be set to 0. Each character of the mask
+ * containing an 'n' signifies a bit of the output that should be set based
+ * on the current increment value of the mask (initially 0). All other
+ * characters in the mask indicate bits that should be unchanged.
+ *
+ * Example of incrementing a mask:
+ *
+ * Mask m = 
+ *   Mask("1111nnnn")
+ * m.apply(01000111)
+ * result: 11110000
+ *
+ * m.increment()
+ * m.apply(01000111)
+ * result: 11110001
+ *
+ * m.increment()
+ * m.apply(01000111)
+ * result: 11110010
+ */
 class Mask {
 
 public:
 
-   Mask(const char* strMask);
+    /*
+     * Construct a Mask object from a string. Each character of the string
+     * determines how a single bit of a buffer will be changed when the mask is
+     * applied.
+     */
+    Mask(const char* strMask);
 
-   ~Mask();
+    /*
+     * Destroys the mask object, freeing memory.
+     */
+    ~Mask();
 
-   void increment(void);
+    /*
+     * Increments the value applied to bits marked with MASK_SYMBOL_INC_BIT.
+     * If the value is at its maximum (for example, a mask with 8 increment
+     * bits with a value of 255), the value will be reset to 0.
+     */
+    void increment(void);
 
-   void apply(char* buf, int bufLen);
+    /*
+     * Applies the mask to a buffer of the given length.
+     */
+    void apply(char* buf, int bufLen);
 
 private:
 
-   char* setMask;
-   char* clrMask;
-   char* incMask;
-   char* incVal;
+    /*
+     * A buffer whose bytes can be ORed with an input buffer to set all bits
+     * that should be 1.
+     */
+    char* setMask;
 
-   int maskLen;
+    /*
+     * A buffer whose bytes can be NANDed with an input buffer to clear all bits
+     * that should be 0.
+     */
+    char* clrMask;
+
+    /*
+     * Each bit in this buffer that contains a 1 will be changed in the input
+     * buffer to reflect 1 bit of the increment value.
+     */
+    char* incMask;
+
+    /*
+     * The value (as a collection of bytes) that should be placed into the
+     * input buffer where the increment mask contains 1s.
+     */
+    char* incVal;
+
+    /*
+     * The length of the mask in bytes.
+     */
+    int maskLen;
 
 };
 
--- fleece/util/Mask.C
+++ fleece/util/Mask.C
@@ -1,63 +1,83 @@
 
+/*
+ * See fleece/COPYRIGHT for copyright information.
+ *
+ * This file is a part of Fleece.
+ *
+ * Fleece is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *  
+ * This software is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, see www.gnu.org/licenses
+*/
+
 #include "Mask.h"
 
-char* getPartialMask(const char* strMask, int maskLen, char symbol) {
-   char* partMask = (char*)malloc(maskLen);
-   assert(partMask != NULL);
-   bzero(partMask, maskLen);
-   
+static char* getPartialMask(const char* strMask, int maskLen, char symbol) {
+    char* partMask = new char[maskLen];
+    assert(partMask != NULL);
+    bzero(partMask, maskLen);
 
-   for (int i = 0; i < 8 * maskLen && strMask[i]; i++) {
-      if (strMask[i] == symbol) {
-         setBufferBit(partMask, i, 1);
-      }
-   }
+    for (int i = 0; i < 8 * maskLen && strMask[i]; i++) {
+        if (strMask[i] == symbol) {
+            setBufferBit(partMask, i, 1);
+        }
+    }
 
-   return partMask;
+    return partMask;
 }
 
 Mask::Mask(const char* strMask) {
-   assert(strMask != NULL && *strMask && "NULL string mask!");
+    assert(strMask != NULL && *strMask && "NULL string mask!");
 
-   maskLen = (strlen(strMask) + 7) / 8;
-   setMask = getPartialMask(strMask, maskLen, MASK_SYMBOL_SET_BIT);
-   clrMask = getPartialMask(strMask, maskLen, MASK_SYMBOL_CLR_BIT);
-   incMask = getPartialMask(strMask, maskLen, MASK_SYMBOL_INC_BIT);
+    maskLen = (strlen(strMask) + 7) / 8;
+    setMask = getPartialMask(strMask, maskLen, MASK_SYMBOL_SET_BIT);
+    clrMask = getPartialMask(strMask, maskLen, MASK_SYMBOL_CLR_BIT);
+    incMask = getPartialMask(strMask, maskLen, MASK_SYMBOL_INC_BIT);
    
-   incVal = (char*)malloc(maskLen);
-   assert(incVal != NULL);
-   bzero(incVal, maskLen);
+    incVal = new char[maskLen];
+    assert(incVal != NULL);
+    bzero(incVal, maskLen);
 }
 
 Mask::~Mask() {
-   free(setMask);
-   free(clrMask);
-   free(incMask);
-   free(incVal);
+    delete[] setMask;
+    delete[] clrMask;
+    delete[] incMask;
+    delete[] incVal;
 }
 
 void Mask::increment(void) {
-   char* cur = incVal + maskLen - 1;
-   while (cur >= incVal && !(++(*cur))) {
-      cur--;
-   }
+    char* cur = incVal + maskLen - 1;
+    while (cur >= incVal && !(++(*cur))) {
+        cur--;
+    }
 }
 
 void Mask::apply(char* buf, int bufLen) {
-   assert(bufLen >= maskLen);
-   
-   // Handle setting and clearing.
-   for (int i = 0; i < maskLen; i++) {
-      buf[i] |= setMask[i];
-      buf[i] &= ~clrMask[i];
-   }
 
-   // Handle the incremented value.
-   int valBit = 8 * maskLen - 1;
-   for (int i = 8 * maskLen - 1; i >= 0; i--) {
-      if (getBufferBit(incMask, i)) {
-         setBufferBit(buf, i, getBufferBit(incVal, valBit));
-         valBit--;
-      }
-   }
+    // Masks can only be applied to buffers of greater or equal length.
+    assert(bufLen >= maskLen);
+   
+    // Handle setting and clearing.
+    for (int i = 0; i < maskLen; i++) {
+        buf[i] |= setMask[i];
+        buf[i] &= ~clrMask[i];
+    }
+
+    // Handle the incremented value.
+    int valBit = 8 * maskLen - 1;
+    for (int i = 8 * maskLen - 1; i >= 0; i--) {
+        if (getBufferBit(incMask, i)) {
+            setBufferBit(buf, i, getBufferBit(incVal, valBit));
+            valBit--;
+        }
+    }
 }
