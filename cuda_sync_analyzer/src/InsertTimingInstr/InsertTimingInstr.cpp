29dd18d64b2c33e91a2c5249d5c0356154d38df1
Nisarg Shah <nisargs@cs.wisc.edu>
2020-02-12 12:21:54 -0600

    Convert exec_times to array

--- cuda_sync_analyzer/src/InsertTimingInstr/InsertTimingInstr.cpp
+++ cuda_sync_analyzer/src/InsertTimingInstr/InsertTimingInstr.cpp
@@ -21,33 +21,33 @@ struct ExecTime {
 // Shared ptr to vector of ExecTimes
 typedef std::shared_ptr<std::vector<ExecTime> > ExecTimesPtr;
 
-class Aggregator {
-private:
-    std::mutex m;
-    uint64_t index = 0;
-public:
-    std::shared_ptr<std::vector<ExecTimesPtr> > aggregates;
-    Aggregator() {
-        aggregates = std::shared_ptr<std::vector<ExecTimesPtr> >(
-            new std::vector<ExecTimesPtr>(100));
-    }
-    void addVec(ExecTimesPtr& v) {
-        std::scoped_lock(m);
-        aggregates->at(index) = v;
-        index++;
-    }
-    size_t size() {
-        return index;
-    }
-};
+// class Aggregator {
+// private:
+//     std::mutex m;
+//     uint64_t index = 0;
+// public:
+//     std::shared_ptr<std::vector<ExecTimesPtr> > aggregates;
+//     Aggregator() {
+//         aggregates = std::shared_ptr<std::vector<ExecTimesPtr> >(
+//             new std::vector<ExecTimesPtr>(100));
+//     }
+//     void addVec(ExecTimesPtr& v) {
+//         std::scoped_lock(m);
+//         aggregates->at(index) = v;
+//         index++;
+//     }
+//     size_t size() {
+//         return index;
+//     }
+// };
 
-std::shared_ptr<Aggregator> agg;
+// std::shared_ptr<Aggregator> agg;
 
 // TODO: how to get size -
 // 1. pass it as argument to entry instrumentation
 // 2. insert it in libcuda and fetch it here
 // 3. use constant value
-thread_local std::shared_ptr<std::vector<ExecTime> > exec_times;
+thread_local ExecTime** exec_times = NULL;
 
 // Maintain count of unresolved API entries
 thread_local uint64_t stack_cnt = 0;
@@ -76,9 +76,15 @@ extern "C" {
         std::ofstream outfile("InstrTimings.out");
         assert(outfile.good());
 
-        for (std::vector<ExecTime>::iterator it = exec_times->begin(); it != exec_times->end(); ++it) {
-            if (it->id == 0) continue;
-            outfile << it->func_name << " " << it->duration << " " << it->sync_duration << " " << it->call_cnt << std::endl;
+        // for (std::vector<ExecTime>::iterator it = exec_times->begin(); it != exec_times->end(); ++it) {
+        //     if (it->id == 0) continue;
+        //     outfile << it->func_name << " " << it->duration << " " << it->sync_duration << " " << it->call_cnt << std::endl;
+        // }
+        for (int i = 0; i < 1000; i++) {
+            if (exec_times[i]->id == 0) continue;
+            outfile << exec_times[i]->func_name << " " << exec_times[i]->duration
+                    << " " << exec_times[i]->sync_duration << " "
+                    << exec_times[i]->call_cnt << std::endl;
         }
     }
 
@@ -88,11 +94,16 @@ extern "C" {
      */
     void DIOG_SignalStartInstra() {
         // std::cout << "Signal start of intrumentation" << std::endl;
-        if (!agg)
-            agg = std::shared_ptr<Aggregator>(new Aggregator);
+        // if (!agg)
+        //     agg = std::shared_ptr<Aggregator>(new Aggregator);
         if (!exec_times) {
-            exec_times = ExecTimesPtr(new std::vector<ExecTime>(1000));
-            agg->addVec(exec_times);
+            //exec_times = ExecTimesPtr(new std::vector<ExecTime>(1000));
+            exec_times = (ExecTime **) malloc(sizeof(ExecTime *) * 1000);
+            for (int i = 0; i < 1000; i++) {
+                exec_times[i] = (ExecTime *) malloc(sizeof(ExecTime));
+            }
+
+            // agg->addVec(exec_times);
         }
 
         if (atexit(DIOG_SAVE_INFO) != 0)
@@ -109,7 +120,7 @@ extern "C" {
         if (stack_cnt > 1) return; // 
 
         // std::cout << "-------Start timer" << std::endl;
-        if (exec_times.get() == NULL)
+        if (exec_times == NULL)
             DIOG_SignalStartInstra();
 
         api_entry = hrc::now();
@@ -129,12 +140,12 @@ extern "C" {
         api_exit = hrc::now();
         // std::cout << "-------Stopped timer for " << name << ", id: " << id << std::endl;
 
-        exec_times->at(id).id = id;
-        exec_times->at(id).duration += std::chrono::duration<double, std::nano>(
+        exec_times[id]->id = id;
+        exec_times[id]->duration += std::chrono::duration<double, std::nano>(
             api_exit - api_entry).count();
-        exec_times->at(id).sync_duration += sync_total;
-        exec_times->at(id).call_cnt++;
-        exec_times->at(id).func_name = name;
+        exec_times[id]->sync_duration += sync_total;
+        exec_times[id]->call_cnt++;
+        exec_times[id]->func_name = name;
 
         sync_total = 0; 
     }
@@ -144,6 +155,8 @@ extern "C" {
      */
     void DIOG_SYNC_ENTRY(uint64_t offset) {
         if (stop_timing) return;
+        // Case when synchronization function is called by a non-public function
+        if (stack_cnt == 0) return;
         // std::cout << "start sync ..." << std::endl;
         sync_entry = hrc::now();
     }
