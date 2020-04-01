eb89266982f1ea90381445341a3e13feccb77198
Nisarg Shah <nisargs@cs.wisc.edu>
2020-04-01 02:24:47 -0500

    Optional support for pretty printing time

--- cuda_sync_analyzer/src/artifacts/pretty_print.py
+++ cuda_sync_analyzer/src/artifacts/pretty_print.py
@@ -1,3 +1,7 @@
+# Usage: <script> <csv file> [-t]
+# [-t] - (optionanl) Print time in seconds, ms, us, etc.
+
+
 import csv
 import sys
 
@@ -5,6 +9,7 @@ import sys
 headers = {}
 titles = []
 perthreadtimes = {}
+fmttime = False
 
 
 def readfromfile():
@@ -34,20 +39,55 @@ def readfromfile():
                 perthreadtimes[curthread].append(row)
 
 
+def transformtime(time):
+    if not fmttime: return("{:,}".format(time))
+
+    timestr = ""
+
+    #minutes = time//6*10**10
+    #if minutes >= 1:
+    #    timestr += str(minutes) + " m"
+    #time %= 6*10**10
+
+    s = time//10**9
+    if s >= 1:
+        timestr += str(s) + "s "
+    time %= 10**9
+
+    ms = time//10**6
+    if ms >= 1:
+        timestr += str(ms) + "ms "
+    time %= 10**6
+
+    us = time//10**3
+    if us >= 1:
+        timestr += str(us) + "us "
+    time %= 10**3
+
+    timestr += str(time) + "ns"
+
+    return(timestr)
+
+
 def justifytext():
+    w1, w2, w3 = 50, 25, 12
     for i in range(len(titles)):
-        if i == 0:
-            titles[i] = titles[i].rjust(60)
+        if i == 0: # Larger padding for API names
+            titles[i] = titles[i].rjust(w1)
+        elif i == 1 or i == 2:
+            titles[i] = titles[i].rjust(w2)
         else:
-            titles[i] = titles[i].rjust(15)
+            titles[i] = titles[i].rjust(w3)
 
     for tid in perthreadtimes:
         for row in perthreadtimes[tid]:
             for i in range(len(row)):
-                if i == 0:
-                    row[i] = row[i].rjust(60)
+                if i == 0: # Larger padding for API names
+                    row[i] = row[i].rjust(w1)
+                elif i == 1 or i == 2:
+                    row[i] = transformtime(int(row[i])).rjust(w2)
                 else:
-                    row[i] = "{:,}".format(int(row[i])).rjust(15)
+                    row[i] = "{:,}".format(int(row[i])).rjust(w3)
 
 
 def prettyprint():
@@ -57,19 +97,23 @@ def prettyprint():
              "HOSTNAME", headers["hostname"]))
     print("%s: %s" % ("TIME", headers["time"]))
 
-    print('\t'.join(titles))
+    print(' '.join(titles))
 
     for tid in perthreadtimes:
         print("THREAD ID: %s" % tid)
 
         for row in perthreadtimes[tid]:
-            print('\t'.join(row))
+            print(' '.join(row))
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        print("Usage: %s <csv file>" % sys.argv[0])
+    if len(sys.argv) not in [2,3]:
+        print("Usage: %s <csv file> [-t]" % sys.argv[0])
+        print("[-t] - (optionanl) Print time in seconds, ms, us, etc.")
+
+    if "-t" in sys.argv:
+        fmttime = True
 
     readfromfile()
     #print(titles)
