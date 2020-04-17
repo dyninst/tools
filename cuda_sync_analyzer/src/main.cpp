165bdafe8fe2bcc9d30108b795aed9216e2bb4f7
Benjamin Welton <welton@cs.wisc.edu>
2020-04-17 17:19:50 -0500

    Split findcudasync from main.cpp, exported function so others can find cuda offset by linking against libcuda_sync_analysis

--- cuda_sync_analyzer/src/main.cpp
+++ cuda_sync_analyzer/src/main.cpp
@@ -2,55 +2,25 @@
 #include "InstrSyncOffset.h"
 #include "LocateCudaSynchronization.h"
 #include "LaunchIdentifySync.h"
+#include "FindCudaSync.h"
 
-std::shared_ptr<DyninstProcess> LaunchApplicationByName(std::string name) {
-    std::shared_ptr<DyninstProcess> ret(new DyninstProcess(name));
-    assert(ret->LaunchProcess() != NULL);
-
-    // Load libcuda.so into the address space of the process
-    BPatch_process *bproc = (BPatch_process *) ret->GetAddressSpace();
-    assert(bproc->isStopped() == true);
-    //ret->LoadLibrary(std::string("libcuda.so.1"));
-    return ret;
-}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <target directory>\n";
         return 1;
     }
+    std::string cudaPath;
 
-    LocateCudaSynchronization scuda;
-    std::vector<uint64_t> potentials;
-    uint64_t syncAddr = scuda.FindLibcudaOffset(false);
+    uint64_t syncAddr = CSA_FindSyncAddress(cudaPath);
+
     std::string newLibcuda(std::string(argv[1]) + std::string("/libcuda.so.1"));
-    if (syncAddr == 0) {
-        potentials = scuda.IdentifySyncFunction();
-        {
-            std::shared_ptr<DyninstProcess> proc = LaunchApplicationByName(
-                    std::string(LOCAL_INSTALL_PATH) + std::string("/bin/hang_devsync"));
-            proc->RunCudaInit();
-            LaunchIdentifySync sync(proc);
-            sync.InsertAnalysis(potentials, std::string("cudaDeviceSynchronize"),
-                    true, std::string("/lib/libFindSyncHelper.so"));
-            proc->RunUntilCompleation();
-            potentials.clear();
-            syncAddr = sync.PostProcessing(potentials);
-            if (potentials.size() > 1) {
-                std::cout << "We have more than one possibility for sync function,"
-                   << " picking lowest level one" << std::endl;
-            }
-            scuda.WriteSyncLocation(syncAddr);
-        }
-    }
-        {
-            std::string libCudaPath = scuda.FindLibCuda().string();
-            std::shared_ptr<BPatchBinary> bin = std::shared_ptr<BPatchBinary>(
-                    new BPatchBinary(libCudaPath, true, newLibcuda));
-            InstrSyncOffset instrSyncOffset(bin);
-            instrSyncOffset.InsertInstr(syncAddr);
-        }
-        std::cout << "Saved instrumented binary to " << newLibcuda << std::endl;
+    std::shared_ptr<BPatchBinary> bin = std::shared_ptr<BPatchBinary>(
+            new BPatchBinary(cudaPath, true, newLibcuda));
+    InstrSyncOffset instrSyncOffset(bin);
+    instrSyncOffset.InsertInstr(syncAddr);
+
+    std::cout << "Saved instrumented binary to " << newLibcuda << std::endl;
         /*
         {
             std::shared_ptr<DyninstProcess> proc = LaunchApplicationByName(
