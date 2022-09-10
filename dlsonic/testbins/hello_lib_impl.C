ceeb198da283b6d79c019e34801be7448e490a5d
Aditya Jain <adityajain.aj10@gmail.com>
2022-09-10 18:58:31 -0500

    add basic dlopen example for testing

---
+++ dlsonic/testbins/hello_lib_impl.C
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "hello_lib_header.H"
+
+void foo()
+{
+	std::cout << "Foo was called - yay!" << std::endl;
+}
---
+++ dlsonic/testbins/trydl.C
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <dlfcn.h>
+
+
+int main()
+{
+	auto hdl = dlopen("libhello.so", RTLD_LAZY);
+	if ( ! hdl ) {
+		std::cerr << "failed to open libtest" << std::endl;
+	}
+	void (*fptr)(void);
+
+	*(void**)(&fptr)  = dlsym(hdl, "_Z3foov");
+
+	char* error = dlerror();
+	if ( error ) {
+		std::cerr << "ERROR: " << std::string(error) << std::endl;
+		return 0;
+	}
+	(*fptr)();
+	dlclose(hdl);
+	return 0;
+}
