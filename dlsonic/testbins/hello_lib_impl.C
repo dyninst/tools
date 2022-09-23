e06a8a38236a14eea5558a3c5e1d883e1990e10e
Aditya Jain <adityajain.aj10@gmail.com>
2022-09-23 17:07:23 -0500

    simplify example binary for simpler code gen

--- dlsonic/testbins/hello_lib_impl.C
+++ dlsonic/testbins/hello_lib_impl.C
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <cstdio>
 #include "hello_lib_header.H"
 
 void foo( const char* msg )
 {
-    std::cout << "Foo was called - yay!: " << std::string(msg) << std::endl;
+    printf("Foo was called: %s\n", msg);
 }
--- dlsonic/testbins/trydl.C
+++ dlsonic/testbins/trydl.C
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <dlfcn.h>
-
+#include <string.h>
 
 void type1()
 {
-    auto hdl = dlopen("libhello.so", RTLD_LAZY);
+    auto hdl = dlopen( "libhello.so", RTLD_LAZY );
     if ( ! hdl ) {
-        std::cerr << "failed to open libhello" << std::endl;
+        printf( "failed to open libhello.so" );
+        return;
     }
     void (*fptr)( const char* );
 
-    *(void**)(&fptr)  = dlsym(hdl, "_Z3fooPKc");
+    *(void**)(&fptr)  = dlsym( hdl, "_Z3fooPKc" );
 
     char* error = dlerror();
     if ( error ) {
-        std::cerr << "ERROR: " << std::string(error) << std::endl;
+        printf( "ERROR: %s", error ); 
         return;
     }
     (*fptr)( "type1" );
-    dlclose(hdl);
+    dlclose( hdl );
 }
 
 void type2()
@@ -26,44 +27,89 @@ void type2()
     const char* libname = "libhello.so";
     auto hdl = dlopen( libname, RTLD_LAZY );
     if ( ! hdl ) {
-        std::cerr << "failed to open libhello" << std::endl;
+        printf( "failed to open libhello.so\n" );
+        return;
     }
     void (*fptr)( const char* );
-    *(void**)(&fptr) = dlsym(hdl, "_Z3fooPKc");
+    *(void**)(&fptr) = dlsym( hdl, "_Z3fooPKc" );
 
     char* error = dlerror();
     if ( error ) {
-        std::cerr << "ERROR: " << std::string(error) << std::endl;
+        printf( "ERROR: %s\n", error ); 
         return;
     }
     (*fptr)( "type2" );
-    dlclose(hdl);
+    dlclose( hdl );
 }
 
 void type3( const char* libname )
 {
     auto hdl = dlopen( libname, RTLD_LAZY );
     if ( ! hdl ) {
-        std::cerr << "failed to open libhello" << std::endl;
+        printf( "failed to open %s\n", libname );
+        return;
     }
     void (*fptr)( const char* );
-    *(void**)(&fptr) = dlsym(hdl, "_Z3fooPKc");
+    *(void**)(&fptr) = dlsym( hdl, "_Z3fooPKc" );
 
     char* error = dlerror();
     if ( error ) {
-        std::cerr << "ERROR: " << std::string(error) << std::endl;
+        printf( "ERROR: %s\n", error ); 
         return;
     }
     (*fptr)( "type3" );
-    dlclose(hdl);
+    dlclose( hdl );
 }
 
+char data[100];
 
+void type4()
+{
+    strcpy(data, "libhello.so");
+    auto hdl = dlopen( data, RTLD_LAZY );
+    if ( ! hdl ) {
+        printf("failed to open libhello.so\n");
+        return;
+    }
+    void (*fptr)( const char* );
+    *(void**)(&fptr) = dlsym( hdl, "_Z3fooPKc" );
+
+    char* error = dlerror();
+    if ( error ) { 
+        printf( "ERROR: %s\n", error ); 
+        return;
+    }
+    (*fptr)( "type4" );
+    dlclose( hdl );
+}
+
+const char* globallibname = "libhello.so";
+
+void type5()
+{
+    auto hdl = dlopen( globallibname, RTLD_LAZY );
+    if ( ! hdl ) {
+        printf("failed to open %s\n", globallibname);
+        return;
+    }
+    void (*fptr)( const char* );
+    *(void**)(&fptr) = dlsym( hdl, "_Z3fooPKc" );
+
+    char* error = dlerror();
+    if ( error ) {
+        printf( "ERROR: %s\n", error );
+        return;
+    }
+    (*fptr)( "type5" );
+    dlclose( hdl );
+}
 
 int main()
 {
     type1();
     type2();
     type3( "libhello.so" );
+    type4();
+    type5();
     return 0;
 }
